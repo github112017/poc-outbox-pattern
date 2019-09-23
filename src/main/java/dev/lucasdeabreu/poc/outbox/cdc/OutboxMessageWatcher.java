5b8016d8965c92f49f222a94f65e4082d812ad10
lucasdeabreu <lucasdeabreu@gmail.com>
2019-09-22 21:52:39 -0300

    Remove unnecessary imports

--- src/main/java/dev/lucasdeabreu/poc/outbox/cdc/OutboxMessageWatcher.java
+++ src/main/java/dev/lucasdeabreu/poc/outbox/cdc/OutboxMessageWatcher.java
@@ -1,7 +1,5 @@
 package dev.lucasdeabreu.poc.outbox.cdc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.lucasdeabreu.poc.outbox.MessageBrokerService;
 import io.debezium.config.Configuration;
 import io.debezium.data.Envelope.Operation;
@@ -18,7 +16,6 @@ import javax.annotation.PreDestroy;
 import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 import static io.debezium.data.Envelope.FieldName.AFTER;
 import static io.debezium.data.Envelope.FieldName.OPERATION;
@@ -28,40 +25,40 @@ import static java.util.stream.Collectors.toMap;
 @Component
 public class OutboxMessageWatcher {
 
-	private final Executor executor = Executors.newSingleThreadExecutor();
+    private final Executor executor = Executors.newSingleThreadExecutor();
 
-	private final EmbeddedEngine engine;
+    private final EmbeddedEngine engine;
 
-	private final MessageBrokerService messageBrokerService;
+    private final MessageBrokerService messageBrokerService;
 
-	public OutboxMessageWatcher(Configuration debeziumConfiguration, MessageBrokerService messageBrokerService) {
-		this.engine = EmbeddedEngine.create().using(debeziumConfiguration).notifying(this::handleEvent).build();
-		this.messageBrokerService = messageBrokerService;
-	}
+    public OutboxMessageWatcher(Configuration debeziumConfiguration, MessageBrokerService messageBrokerService) {
+        this.engine = EmbeddedEngine.create().using(debeziumConfiguration).notifying(this::handleEvent).build();
+        this.messageBrokerService = messageBrokerService;
+    }
 
-	@PostConstruct
-	void postConstruct() {
-		executor.execute(engine);
-	}
+    @PostConstruct
+    void postConstruct() {
+        executor.execute(engine);
+    }
 
-	@PreDestroy
-	void stop() {
-		if (engine != null) {
-			engine.stop();
-		}
-	}
+    @PreDestroy
+    void stop() {
+        if (engine != null) {
+            engine.stop();
+        }
+    }
 
-	private void handleEvent(SourceRecord sourceRecord) {
-		Struct sourceRecordValue = (Struct) sourceRecord.value();
-		Operation operation = Operation.forCode((String) sourceRecordValue.get(OPERATION));
-		if (operation == Operation.CREATE) {
-			Struct after = (Struct) sourceRecordValue.get(AFTER);
-			Map<String, String> message = after.schema().fields().stream().map(Field::name)
-					.map(fieldName -> Pair.of(fieldName, after.getString(fieldName)))
-					.collect(toMap(Pair::getKey, Pair::getValue));
-			log.debug("Sending to message broker: {}", message);
-			messageBrokerService.send("TOPIC", message);
-		}
-	}
+    private void handleEvent(SourceRecord sourceRecord) {
+        Struct sourceRecordValue = (Struct) sourceRecord.value();
+        Operation operation = Operation.forCode((String) sourceRecordValue.get(OPERATION));
+        if (operation == Operation.CREATE) {
+            Struct after = (Struct) sourceRecordValue.get(AFTER);
+            Map<String, String> message = after.schema().fields().stream().map(Field::name)
+                    .map(fieldName -> Pair.of(fieldName, after.getString(fieldName)))
+                    .collect(toMap(Pair::getKey, Pair::getValue));
+            log.debug("Sending to message broker: {}", message);
+            messageBrokerService.send("TOPIC", message);
+        }
+    }
 
 }
